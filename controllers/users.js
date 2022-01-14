4596cf151f4af402dbcf31964f67cf77feb6093e
Alexis <alexismayaglez@gmail.com>
2022-01-13 20:21:07 -0600

    primer commit

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,23 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
-let users = [];
+let users = [{
+
+    id: uuidv4(),
+    firstName: "Alexis", 
+    lastName: "Maya", 
+    age: 26
+},
+{
+    id: uuidv4(),
+    firstName: "Pamela", 
+    lastName: "Rosas", 
+    age: 16
+},
+
+];
 
 export const getUsers = (req, res) => {
-    console.log(`Users in the database: ${users}`);
+    console.log(`Usuarios en la base: ${users}`);
 
     res.send(users);
 }
@@ -11,26 +25,47 @@ export const getUsers = (req, res) => {
 export const createUser = (req, res) => {   
     const user = req.body;
 
-    users.push({...user, id: uuid()});
+    users.push({...user, id: uuidv4()});
     
-    console.log(`User [${user.username}] added to the database.`);
+    console.log(`Usuario [${user.username}] añadido a la base.`);
+
+    res.send(`Usuario con el nombre ${user.firstName} añadido a la base`); 
 };
 
 export const getUser = (req, res) => {
-    res.send(req.params.id)
+    const {id} = req.params;
+
+    const foundUser = users.find((user) => user.id === id); 
+
+    res.send(foundUser); 
 };
 
 export const deleteUser = (req, res) => { 
-    console.log(`user with id ${req.params.id} has been deleted`);
+
+    const {id} = req.params;
+
+    users = users.filter((user) => user.id !== id);
     
-    users = users.filter((user) => user.id !== req.params.id);
+    console.log(`El usuario con el id: ${id} ha sido borrado`);
+
+    res.send(`Usuario con el id ${id} borrado de la base`); 
 };
 
 export const updateUser =  (req,res) => {
-    const user = users.find((user) => user.id === req.params.id);
+
+    const {id} = req.params; 
+    const {firstName, lastName, age} = req.body; 
+
+    const user = users.find((user) => user.id === id);
+
+    if(firstName) user.firstName = firstName; 
+    if(lastName) user.lastName = lastName; 
+    if(age) user.age = age; 
     
     user.username = req.body.username;
     user.age = req.body.age;
 
-    console.log(`username has been updated to ${req.body.username}.age has been updated to ${req.body.age}`)
+    console.log(`Usuario con el id ${id} ha sido actualizado`); 
+
+    res.send(`Usuario con el id ${id} ha sido actualizado`); 
 };
--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import express from "express";
-import bodyParser from "body-parser";
+
 
 import usersRoutes from "./routes/users.js";
+
+import cors from 'cors'
 
 const app = express();
 const PORT = 5000;
 
-app.use(bodyParser.json());
+app.use(cors())
+
+app.use(express.json());
 
 app.use("/people", usersRoutes);
 app.get("/", (req, res) => res.send("Welcome to the Users API!"));
-app.all("*", (req, res) =>res.send("You've tried reaching a route that doesn't exist."));
+app.all("*", (req, res) =>res.send("No existe la ruta"));
 
-app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
+app.listen(PORT, () =>console.log(`Servidor corriendo en el puerto: http://localhost:${PORT}`));
